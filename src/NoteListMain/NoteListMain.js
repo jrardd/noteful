b43f28c1b8fa6e689ff573ee9342afeabcc397dc
Kareine Elecanal <kaelecanal@gmail.com>
2021-01-12 22:11:06 -0800

    added missing file

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Note from "../Note/Note";
 import CircleButton from "../CircleButton/CircleButton";
 import "./NoteListMain.css";
